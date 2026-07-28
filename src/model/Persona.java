d166f7cf59e91c9d030d9670d818e0a86ff54827
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-28 12:24:22 -0500

    commit - Persona .

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -1,5 +1,75 @@
 package model;
 
-public class Persona
-{
-}
+public class Persona {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String ciudad;
+
+    // constructor vacio .
+    public Persona() {
+
+    }
+
+    // constructor con parametros.
+    public Persona(int id, String nombre, String apellido, int edad, String ciudad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.ciudad = ciudad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", edad=" + edad +
+                ", ciudad='" + ciudad + '\'' +
+                '}';
+    }
+}   
