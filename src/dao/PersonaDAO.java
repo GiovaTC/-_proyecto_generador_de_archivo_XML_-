9c23ef1742f2a4fc2dbb053650f8c5a7459e04f4
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-28 12:52:37 -0500

    commit - Persona .

--- src/dao/PersonaDAO.java
+++ src/dao/PersonaDAO.java
@@ -1,5 +1,47 @@
 package dao;
 
-public class PersonaDAO
-{
-}
+import config.ConexionBD;
+import model.Persona;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class PersonaDAO {
+
+    public ArrayList<Persona> obtenerPersonas() {
+        ArrayList<Persona> lista = new ArrayList<>();
+
+        String sql = "SELECT * FROM personas";
+
+        try {
+            Connection conexion = ConexionBD.conectar();
+
+            PreparedStatement ps = conexion.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Persona persona = new Persona();
+
+                persona.setId(rs.getInt("id"));
+                persona.setNombre(rs.getString("nombre"));
+                persona.setApellido(rs.getString("apellido"));
+                persona.setEdad(rs.getInt("edad"));
+                persona.setCiudad(rs.getString("ciudad"));
+
+                lista.add(persona);
+            }
+
+            rs.close();
+            ps.close();
+            conexion.close();
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return lista;
+    }
+}   
--- src/model/Persona.java
+++ src/model/Persona.java
@@ -72,4 +72,4 @@ public class Persona {
                 ", ciudad='" + ciudad + '\'' +
                 '}';
     }
-}   
+}
